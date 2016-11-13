049d055928b61703c071d532b59e023317463c00
Connor Brennan <ctbrennan@berkeley.edu>
2016-11-12 20:19:37 -0800

    added bodyparser

--- server.js
+++ server.js
@@ -331,7 +331,7 @@ app.get("/topics/list", function(req, res){
 
 
 
-var server = app.listen(8097, function(){
+var server = app.listen(env.PORT, function(){
 	var host = server.address().address
 	var port = server.address().port
 	console.log("Example app listening at http://%s:%s", host, port)
